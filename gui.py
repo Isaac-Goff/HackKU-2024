a5bcf5a93208f5547fae0147d0e24e4872549b11
red2922 <inthavongsaj@lopers.unk.edu>
2024-04-13 14:15:12 -0500

    Added a little more functionality

--- gui.py
+++ gui.py
@@ -5,19 +5,26 @@ from tkinter import *
 import os
 
 
+
 def open_window():
     window = Tk()
+    window.attributes("-alpha", 0.4)
     window.title("Falsified")
     s_width = window.winfo_screenwidth()
     s_height = window.winfo_screenheight()
+    window.geometry('%dx%d+%d+%d' % (s_width / 5, s_height / 6, 1520, 150))
+    window.resizable(False, False)
 
     frame = Frame(window)
     frame.pack()
 
     check = Button(frame, text="Check Article")
+
+    close = Button(frame, text="Close", command=window.destroy)
+    close.pack(side=BOTTOM, padx=20, pady=30)
     check.pack(side=BOTTOM, padx=20, pady=30)
 
-    window.geometry('%dx%d+%d+%d' % (s_width / 5, s_height / 6, 1520, 150))
+    window.protocol("WM_DELETE_WINDOW", 'disable_event')
     window.mainloop()
 
 
