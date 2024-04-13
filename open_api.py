7c1e9a330f39b88301d66f517b283223572f81d8
red2922 <inthavongsaj@lopers.unk.edu>
2024-04-13 00:50:01 -0500

    created the nice open ai class

--- open_api.py
+++ open_api.py
@@ -2,19 +2,33 @@ from openai import OpenAI
 import os
 from dotenv import load_dotenv, find_dotenv
 
-#System helps set the tone for the Assistant. It is optional
-#User will provide requests or comments for the assistant to respond to. Kinda like the regular API 
-#Assistant are model responses
-        
+# System helps set the tone for the Assistant. It is optional
+# User will provide requests or comments for the assistant to respond to. Kinda like the regular API
+# Assistant are model responses
 env = load_dotenv(find_dotenv())
 connect = OpenAI()
 connect.api_key = os.getenv("OPENAI_API_KEY")
-response = connect.chat.completions.create(
-  model="gpt-4-turbo",
-  messages=[
-    {"role": "system", "content": "You are a unbiased assistant."},
-    {"role": "user", "content": "Who won the world series in 2020?"},
-  ]
-)
 
-print(response.choices[0].message.content)
+
+class Api:
+
+    def __init__(self, message, model):
+        self._message = message
+        self._model = model
+
+    def get_message(self):
+        return self._message()
+
+    def set_message(self, new: str):
+        self._message = new
+
+    def send_message(self):
+        response = connect.chat.completions.create(
+            model="gpt-4-turbo",
+            messages=[
+                {"role": "system", "content": "You are a unbiased assistant."},
+                {"role": "user", "content": f"{self.get_message()}"},
+            ]
+        )
+
+        return response.choices[0].message.content
